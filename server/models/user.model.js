60c4c084613071e2d6db0653c2b752fc0ae3c073
Punith Achintha <punithachintha@gmail.com>
2025-01-30 18:10:05 +0530

    Initial commit

---
+++ server/models/user.model.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name : {
+        type : String,
+        required : [true,"Provide Name"]
+    },
+    email : {
+        type : String,
+        required : [true,"Provide Email"],
+        unique : true
+    },
+    password : {
+        type : String,
+        required : [true,"Provide PAssword"]
+    },
+    refresh_token : {
+        type : String,
+        default : ""
+    },
+    verify_email : {
+        type : Boolean,
+        default : false
+    },
+    status : {
+        type : String,
+        enum : ["Active","Inactive","Suspended"],
+        default : "Active"
+    },
+    forgot_password_otp : {
+        type : String,
+        default : null
+    },
+    forgot_password_expiry : {
+        type : Date,
+        default : ""
+    },
+    role : {
+        type : String,
+        enum : ['ADMIN',"USER"],
+        default : "USER"
+    }
+},{
+    timestamps : true
+})
+
+const UserModel = mongoose.model("User",userSchema)
+
+export default UserModel

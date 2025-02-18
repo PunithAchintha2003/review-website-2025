ef969acfa861031f8bbb17a21a63ad57ec46ab6c
Punith Achintha <punithachintha@gmail.com>
2025-02-18 13:18:43 +0530

    Client 1st commit

---
+++ client/src/component/Header.jsx
@@ -0,0 +1,15 @@
+const Header = () => {
+  return (
+    <div>
+        <header className="h-15 shadow-md sticky top-0">
+            {/* Logo */}
+
+            {/* Search */}
+            
+            {/* Category */}
+        </header>
+    </div>
+  )
+}
+
+export default Header

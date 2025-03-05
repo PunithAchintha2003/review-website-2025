f0e8d1d6e8b3cfb55d0de42591a64f623bcf1847
Punith Achintha <punithachintha@gmail.com>
2025-03-05 12:27:04 +0530

    2nd commit

--- client/src/component/Header.jsx
+++ client/src/component/Header.jsx
@@ -1,12 +1,29 @@
+import { useNavigate } from 'react-router-dom'
+
 const Header = () => {
+
+  const navigate = useNavigate
+
+  const redirectToLoginPage = () => {
+    navigate("/login")
+  }
+
   return (
     <div>
         <header className="h-15 shadow-md sticky top-0">
-            {/* Logo */}
+            <div className="container mx-auto flex items-center h-full px-4 justify-between">
+              {/* Logo */}
 
-            {/* Search */}
-            
-            {/* Category */}
+              {/* Search */}
+              <div>
+                Search
+              </div>
+
+              {/* Login */}
+              <div className="hidden lg:flex items-center">
+                <button onClick={redirectToLoginPage} className='text-lg px-2 cursor-pointer'>Login</button>
+              </div>
+            </div>
         </header>
     </div>
   )

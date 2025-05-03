a57f66e95a2f23fca800a983845e124ba88e7d1a
Panagoda P R K <prkpanagoda@students.nsbm.ac.lk>
2025-05-03 17:33:45 +0530

    remove green background color of greengrass

--- client/src/layouts/Dashboard.jsx
+++ client/src/layouts/Dashboard.jsx
@@ -7,7 +7,7 @@ const Dashboard = () => {
 
     console.log("user dashboard",user)
   return (
-        <section className="bg-green-100">
+        <section>
             <div className="container mx-auto p-3  grid lg:grid-cols-[250px_1fr]">
                     {/* left for menu*/}
                     <div className="py-4 sticky top-15 max-h-[calc(100vh-60px)] overflow-y-auto hidden lg:block border-r border-slate-400">
@@ -15,7 +15,7 @@ const Dashboard = () => {
                     </div>
 
                     {/* right for content*/}
-                    <div className="bg-green-100 min-h-[82vh] rounded-md p-4">
+                    <div className="min-h-[82vh] rounded-md p-4">
                         <Outlet/>
                     </div>
             </div>

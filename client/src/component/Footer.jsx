d0e3e45365307341ad44d4380cd6a757fde3f119
Panagoda P R K <prkpanagoda@students.nsbm.ac.lk>
2025-05-03 17:32:30 +0530

    centered footer text made background black. footer now sticks to the bottom of the page

--- client/src/component/Footer.jsx
+++ client/src/component/Footer.jsx
@@ -4,13 +4,21 @@ import { FaLinkedin } from "react-icons/fa";
 
 const Footer = () => {
   return (
+    <footer
+      style={{
+        backgroundColor: "black",
+        color: "white",
+        textAlign: "center",
+        position: "fixed",
+        bottom: 0,
+        width: "100%",
+      }}
+      className="border-t"
+    >
+      <div className="container mx-auto p-2 text-center">
+        <p>© 2024 GreenGrass. All rights reserved.</p>
 
-    // bg-slate-100 text-black
-    <footer className="border-t">
-        <div className="container mx-auto p-4 text-center flex flex-col lg:flex-row lg:justify-between gap-2">
-            <p>© All Rights Reserved  2025.</p>
-
-            <div className="flex items-center gap-4 justify-center text-2xl">
+        {/* <div className="flex items-center gap-4 justify-center text-2xl">
                 <a href="" className="hover:text-green-400">
                     <FaFacebook/>
                 </a>
@@ -20,10 +28,10 @@ const Footer = () => {
                 <a href="" className="hover:text-green-400">
                     <FaLinkedin/>
                 </a>
-            </div>
-        </div>
+            </div> */}
+      </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
